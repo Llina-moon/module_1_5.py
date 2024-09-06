d0b4249bd3aeb205fa6b5de4c8dabb45c0d93b8d
Llina-moon <llina.moon.luna@gmail.com>
2024-09-06 17:14:41 +0300

    Add files via upload

---
+++ module_1_5.py
@@ -0,0 +1,10 @@
+immutable_var= (24, [1,7],True,'cat')
+print(immutable_var)
+
+immutable_var [1][0]= 3
+print(immutable_var)
+
+#кортеж является не изменяемой конструкцией,однако элементы можно изменить,например,значение в списке
+
+mutable_list = [1,2,38,'banana']
+print(mutable_list)
