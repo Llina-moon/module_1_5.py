8df8b725e32c3c8579c93c160236c2aeff8449a9
Llina-moon <llina.moon.luna@gmail.com>
2024-09-07 11:22:46 +0300

    module_1_5.py

--- module_1_5.py
+++ module_1_5.py
@@ -7,4 +7,6 @@ print(immutable_var)
 #кортеж является не изменяемой конструкцией,однако элементы можно изменить,например,значение в списке
 
 mutable_list = [1,2,38,'banana']
+mutable_list [2] = True
+mutable_list.extend(['cola',000,5.6])
 print(mutable_list)
